d263267a3100ae27793044a0e16303a82b3789b7
tm-umino <t.umino@tcmobile.jp>
2017-11-28 13:52:26 +0900

    バリデーションを追加

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,4 +1,7 @@
 class News < ApplicationRecord
+  validates :title, presence:true
+  validates :contents, presence: true
+
 
   def self.getNews(isMember = false)
     if (isMember)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ require 'digest/md5'
 
 class User < ApplicationRecord
   has_secure_password validations: true
+  validates :name, presence:true
+  validates :email, presence: true
 
 end
